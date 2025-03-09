d457cba93aa7365cfebabcc323e65e3c210a53c1
photomanai <photom4n.41@gmail.com>
2025-03-10 00:16:51 +0400

    compression

--- main.cpp
+++ main.cpp
@@ -2,23 +2,52 @@
 #include <fstream>
 #include <bitset>
 
-int main() {
-    std::ifstream file("yourfile.txt");  // Open text file
-    std::ofstream write("writefile.bin", std::ios::binary);  // Open binary file
+std::ifstream file("yourfile.txt");  // Open text file
+std::ofstream write("writefile.bin", std::ios::binary);  // Open binary file
 
+
+void compression(std::string binary) {
+    std::string zip_line;
+    int count = 1;  
+
+    for (size_t i = 0; i < binary.size() - 1; ++i) {
+        if (binary[i] == binary[i + 1]) {
+            count++;  
+        } else {
+
+			std::cout << count << " " << binary[i] << std::endl;
+            
+            zip_line += std::to_string(count) + binary[i];
+            count = 1;  
+        }
+    }
+
+    // For the last group of bits (because the loop stops before the last bit)
+    zip_line += std::to_string(count) + binary[binary.size() - 1];
+
+    std::cout << "Compressed: " << zip_line << std::endl;
+    write<<zip_line;
+}
+
+int main() {
+	std::string bin_line;
+	std::bitset<8> binary(0);
+	
     if (file.is_open() && write.is_open()) {  // Ensure both files are open
         std::string line;
         while (getline(file, line)) {
             std::cout << "Text: " << line << std::endl;
             std::cout << "Binary: " << std::endl;
-            write << std::endl;
 
             for (char c : line) {
-                std::bitset<8> binary(c);  // Convert char to 8-bit binary
-                std::cout << binary << " ";
-                write << binary << " ";  // Write to file
+                std::bitset<8> binary(c);  // Convert char to 8-bit binary				
+                 bin_line += binary.to_string(); 
             	//write.write(reinterpret_cast<const char*>(&c), sizeof(c));
-	    }
+	    	}
+	    	compression(bin_line);
+	    	
+	    	write << std::endl;
+	    	
             std::cout << std::endl;
         }
         file.close();
