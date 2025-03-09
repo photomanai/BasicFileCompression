8bee9b59d20b9bec930744109ab89449ce3a99aa
photomanai <photom4n.41@gmail.com>
2025-03-09 16:53:26 +0400

    README.md updated

--- main.cpp
+++ main.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
+#include <fstream>
+#include <bitset>
 
-int main(){
-	std::cout<<"Helloo c++";
-	return 0;
+int main() {
+    std::ifstream file("yourfile.txt");  // Open text file
+    std::ofstream write("writefile.bin", std::ios::binary);  // Open binary file
+
+    if (file.is_open() && write.is_open()) {  // Ensure both files are open
+        std::string line;
+        while (getline(file, line)) {
+            std::cout << "Text: " << line << std::endl;
+            std::cout << "Binary: " << std::endl;
+            write << std::endl;
+
+            for (char c : line) {
+                std::bitset<8> binary(c);  // Convert char to 8-bit binary
+                std::cout << binary << " ";
+                write << binary << " ";  // Write to file
+            	//write.write(reinterpret_cast<const char*>(&c), sizeof(c));
+	    }
+            std::cout << std::endl;
+        }
+        file.close();
+        write.close();
+    } else {
+        std::cout << "File is not here bro" << std::endl;
+    }
+
+    return 0;
 }
+
